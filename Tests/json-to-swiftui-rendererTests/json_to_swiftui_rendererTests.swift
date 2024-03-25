91cb1d89713213cb8d9e6e97e1d11ba14cc1d7a2
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-03-25 20:03:06 +0800

    ⚙️ Chore: Run `swift package init`
    Initial commit

---
+++ Tests/json-to-swiftui-rendererTests/json_to_swiftui_rendererTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import json_to_swiftui_renderer
+
+final class json_to_swiftui_rendererTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
