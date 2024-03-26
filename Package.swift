00f75c3587b46e5791d2c957e912cb2543db1870
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-03-26 14:54:01 +0800

    💄 Gloss: Cleanup SPM Config

--- Package.swift
+++ Package.swift
@@ -1,23 +1,22 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "json-to-swiftui-renderer",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "json-to-swiftui-renderer",
-            targets: ["json-to-swiftui-renderer"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "json-to-swiftui-renderer"),
-        .testTarget(
-            name: "json-to-swiftui-rendererTests",
-            dependencies: ["json-to-swiftui-renderer"]),
-    ]
+  name: "json-to-swiftui-renderer",
+  products: [
+    .library(
+      name: "json-to-swiftui-renderer",
+      targets: ["json-to-swiftui-renderer"]
+    ),
+  ],
+
+targets: [
+  .target(
+    name: "json-to-swiftui-renderer"),
+    .testTarget(
+      name: "json-to-swiftui-rendererTests",
+      dependencies: ["json-to-swiftui-renderer"]
+    ),
+  ]
 )
