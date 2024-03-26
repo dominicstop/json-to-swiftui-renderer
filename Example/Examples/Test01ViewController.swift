0aff69773371a9e886939bf7b7844b2ba12e9ae0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-03-26 15:28:28 +0800

    💫 Update: Ex - Setup SPM Example Project

---
+++ Example/Examples/Test01ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  Test01ViewController.swift
+//  SwiftUtilitiesExample
+//
+//  Created by Dominic Go on 11/29/23.
+//
+
+import UIKit
+
+fileprivate class ModalViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad();
+    
+    self.view.backgroundColor = .white;
+  };
+};
+
+class Test01ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad();
+    
+    self.view.backgroundColor = .white;
+  };
+};
