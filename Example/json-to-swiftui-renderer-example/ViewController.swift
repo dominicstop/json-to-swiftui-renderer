21e1c25f9c3ef684763952f67e12b5c1bd4560b7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-03-26 15:08:11 +0800

    ⭐️ Impl: Create SPM Example Project

---
+++ Example/json-to-swiftui-renderer-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  json-to-swiftui-renderer-example
+//
+//  Created by Dominic Go on 3/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
